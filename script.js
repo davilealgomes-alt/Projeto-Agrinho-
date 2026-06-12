8b40e47082b697c3390cc771e71e87214044975f
davilealgomes-alt <davi.leal.gomes@escola.pr.gov.br>
2026-06-12 08:38:42 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Site Agro Forte, Futuro Sustentável carregado com sucesso!");
+
+let ano = new Date().getFullYear();
+
+console.log("Ano atual: " + ano);
+
+let mensagem = "Bem-vindo ao Projeto Agrinho!";
+console.log(mensagem);
+
+alert("Bem-vindo ao projeto Agro Forte, Futuro Sustentável!");
